4a4926f90c8bb80f6380345e192b8a3c88ea7a45
Crisman Carmona <crisman999@outlook.com>
2024-07-26 00:06:07 -0500

    .

--- src/domain/plane.ts
+++ src/domain/plane.ts
@@ -2,9 +2,24 @@ export class Plane {
   currentPosition?: { x: number; y: number; z: number } = { x: 0, y: 0, z: 0 };
   isOn?: boolean = false;
   intervals?: NodeJS.Timeout[] = [];
-  port?: string;
+  updatedAt?: Date;
 
-  constructor(readonly id: string, readonly numberId: number) {}
+  constructor(
+    readonly id: string,
+    readonly numberId: number,
+    readonly port: string
+  ) {}
+
+  update() {
+    this.updatedAt = new Date();
+  }
+
+  getDTO(): PlaneDto {
+    const dto = { ...this };
+    delete dto.intervals;
+
+    return dto;
+  }
 }
 
 export class PlaneDto implements Partial<Plane> {}
